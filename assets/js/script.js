247ccbf2b2393e49c8f2f53d9e0bdce6a9abbbc3
d-taylor6403 <dannielletaylor@outlook.com>
2021-01-16 12:35:32 -0600

    adding touch punch for mobile functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -224,7 +224,22 @@ $(".card .list-group").sortable({
     tasks[arrName] = tempArr;
     saveTasks();
 
-    console.log(tempArr);
+    //console.log(tempArr);
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop")
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
   }
 });
 
